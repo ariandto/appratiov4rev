042239dedc082a130dd9c31d62f2fbdd7ca705c8
ariandto <92358210+ariandto@users.noreply.github.com>
2022-09-11 01:09:46 +0700

    3 files modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var km = document.getElementById('kilometertempuh');
 var liter = document.getElementById('totalliter');
-var hasilnya = document.getElementById('hasil');
+var hasilnya = document.getElementById('hasilratio');
 
 function fratio() {
   hasilnya.value = Number(km.value) / Number(liter.value);
