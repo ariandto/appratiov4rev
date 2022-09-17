589f488cf60be93339d9ee23b8b6b7598ab3a435
ariandto <92358210+ariandto@users.noreply.github.com>
2022-09-17 20:44:54 +0700

    disable km tempuh

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var liter = document.getElementById('totalliter');
 var hasilnya = document.getElementById('hasilratio');
 const tombol = document.querySelector('button');
 document.getElementById('hasilratio').disabled = true;
+document.getElementById('kilometertempuh').disabled = true;
 
 function fratio() {
   hasilnya.value = Number(km.value) / Number(liter.value);
